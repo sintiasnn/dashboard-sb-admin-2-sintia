e8b95be851ec932a6514b1c4ee8152daa8143122
sinsin <62082948+sintiasnn@users.noreply.github.com>
2022-11-08 08:55:41 +0800

    editing chart view

--- resources/views/chartjs.blade.php
+++ resources/views/chartjs.blade.php
@@ -23,7 +23,7 @@
   </div>
 
   <div class="col-xl-8 col-lg-7">
-    <div class="card shadow mb-4" style=" height:30%; width:30%">
+    <div class="card shadow mb-4" style=" height:50%; width:50%">
       <div class="card-header py-3">
         <h6 class="m-0 font-weight-bold text-primary">Indeks A1 </h6>
       </div>
@@ -33,8 +33,8 @@
     </div>
   </div>
 
-  <div class="col-xl-4 col-lg-5">
-    <div class="card shadow mb-4" style=" height:25%; width:25% ">
+  <div class="col-xl-8 col-lg-7">
+    <div class="card shadow mb-4" style=" height:50%; width:50% ">
       <div class="card-header py-3">
         <h6 class="m-0 font-weight-bold text-primary">Indeks A2 </h6>
       </div>
