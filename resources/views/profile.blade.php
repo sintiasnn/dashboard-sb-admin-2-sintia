02e04e4bf8a4b8187cbe47ae59bea22d2d9314e8
sinsin <62082948+sintiasnn@users.noreply.github.com>
2022-11-10 08:31:28 +0800

    editing profile bladde

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -42,26 +42,6 @@
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="card-profile-stats">
-                                <span class="heading">22</span>
-                                <span class="description">Friends</span>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card-profile-stats">
-                                <span class="heading">10</span>
-                                <span class="description">Photos</span>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card-profile-stats">
-                                <span class="heading">89</span>
-                                <span class="description">Comments</span>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
 
